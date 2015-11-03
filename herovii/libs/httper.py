754c8dcd67ac2ee3ed6395143cb0e6068a1a3f52
hiyouth <aswindguy@outlook.com>
2015-11-03 19:49:56 +0800

    finish http post module

--- herovii/libs/httper.py
+++ herovii/libs/httper.py
@@ -1,8 +1,7 @@
 __author__ = 'bliss'
 
 from urllib import request as httpreq
-import http.client
-from urllib.error import HTTPError
+from http.client import HTTPSConnection, HTTPResponse
 from flask import json
 
 
@@ -12,35 +11,31 @@ class Httper(object):
         r = httpreq.urlopen(req)
         print(r)
 
-    def post(self, url, data, headers=None):
+    def post(self, host,url, data, headers=None):
         tmp_data = json.dumps(data)
         tmp_data = tmp_data.encode(encoding='utf-8')
-        req = httpreq.Request(url=url,
-                              data=tmp_data, method='POST')
-        if headers is not None:
-            req.headers = headers
-
-        try:
-            r = httpreq.urlopen(req, timeout=120)
-        except HTTPError as e:
-            s = e
-
-        # r = request.urlopen('http://sina.com')
-        rlt = r.read()
-        print(rlt)
-        return r
+        # req = httpreq.Request(url=url,
+        #                       data=tmp_data, method='POST')
+        # if headers is not None:
+        #     req.headers = headers
+        con = HTTPSConnection(host)
+        con.request("POST", url, body=tmp_data, headers=headers)
+        r = con.getresponse()
+        res = r.read()
+        return  res
 
 
 class BMOB(Httper):
     # 短信服务商参数配置
     APP_ID = '8bee8ad5acaaa664f2f644c9e3a37c2e'
-    API_KEY = '62060704177b04ee83d1bbc3778d5051111'
+    API_KEY = '62060704177b04ee83d1bbc3778d5051'
     API_SMS_CODE = r'https://api.bmob.cn/1/requestSmsCode'
+    API_HOST = "api.bmob.cn"
 
     def send_verify_sms(self, phone_number):
         header_dic = {'X-Bmob-Application-Id': BMOB.APP_ID,
                       'X-Bmob-REST-API-Key': BMOB.API_KEY,
                       'Content-Type': 'application/json'}
         post_data = {'mobilePhoneNumber': phone_number}
-        r = self.post(BMOB.API_SMS_CODE, post_data, header_dic)
-        print(r)
+        response = self.post(BMOB.API_HOST, BMOB.API_SMS_CODE, post_data, header_dic)
+        status = response.status
