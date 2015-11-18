7c988bdb5f0844f000e316fbfb8032bdea8d49bd
hiyouth <499055803@qq.com>
2015-11-19 00:43:39 +0800

    调整代码格式，未改动代码

--- tests/api/test_news_org.py
+++ tests/api/test_news_org.py
@@ -2,15 +2,9 @@ from tests.api._base import TestNewsOrgCase
 
 __author__ = 'bliss'
 
-import unittest
-from ._base import TestCase
-
 
 class TestOrgNews(TestNewsOrgCase):
     def test_org_news_paging(self):
         url = '/v1/news/org?page=1&count=2'
         rv = self.client.get(url)
-        print(rv.data)
         self.assertEqual(rv.status_code, 200)
-
-
