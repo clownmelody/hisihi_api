2fbd77c406ef9dedf843696a695bcccd92141bce
hiyouth <aswindguy@outlook.com>
2015-11-19 19:44:21 +0800

    coding

--- tests/api/test_news_org.py
+++ tests/api/test_news_org.py
@@ -7,5 +7,6 @@ class TestOrgNews(TestNewsOrgCase):
     def test_org_news_paging(self):
         """News：对Org的新闻查新接口做测试"""
         url = '/v1/news/org?page=1&count=2'
-        rv = self.client.get(url)
+        headers = self.get_authorized_header()
+        rv = self.client.get(url, headers=headers)
         self.assertEqual(rv.status_code, 200)
