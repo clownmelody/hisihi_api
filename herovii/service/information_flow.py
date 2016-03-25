4cd02451a5cafd472ca9658b9f85c5789c57db7e
yangchujie <yangchujie1@163.com>
2016-03-25 11:38:35 +0800

    资讯流banner增加位置筛选；

--- herovii/service/information_flow.py
+++ herovii/service/information_flow.py
@@ -24,12 +24,14 @@ __author__ = 'yangchujie'
 
 # 分页获取资讯流 banner 列表
 def get_information_flow_banner_service(page, per_page):
-    banner_count = db.session.query(InformationFlowBanner).filter(InformationFlowBanner.status == 1).count()
+    banner_count = db.session.query(InformationFlowBanner).filter(InformationFlowBanner.status == 1,
+                                                                  InformationFlowBanner.show_pos == 1).count()
     data_list = []
     start = (page - 1) * per_page
     stop = start + per_page
     banner_list = db.session.query(InformationFlowBanner) \
-        .filter(InformationFlowBanner.status == 1) \
+        .filter(InformationFlowBanner.status == 1,
+                InformationFlowBanner.show_pos == 1) \
         .slice(start, stop) \
         .all()
     if banner_list:
