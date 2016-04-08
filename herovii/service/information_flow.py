b4c8f685719145f1f1928a301f1fc879f04faa7c
shaolei <ysl90s@qq.com>
2016-04-08 15:27:43 +0800

    增加资讯流获取机构课程时机构为空的判断

--- herovii/service/information_flow.py
+++ herovii/service/information_flow.py
@@ -192,27 +192,30 @@ def get_course_info_by_id(course_id):
     organization_course = db.session.query(Course).filter(Course.id == course_id) \
         .first()
     organization_info = get_organization_info_by_organization_id(organization_course.organization_id)
-    if organization_course:
-        lecturer_id = organization_course['lecturer']
-        user_profile = get_user_profile_by_uid(lecturer_id)
-        if user_profile is not None:
-            lecturer_nickname = user_profile['nickname']
+    if organization_info:
+        if organization_course:
+            lecturer_id = organization_course['lecturer']
+            user_profile = get_user_profile_by_uid(lecturer_id)
+            if user_profile is not None:
+                lecturer_nickname = user_profile['nickname']
+            else:
+                lecturer_nickname = ''
+            type_str = get_course_type_name_by_type_id(organization_course['category_id'])
+            duration = get_course_video_duration(course_id)
+            course = {
+                'id': course_id,
+                'title': organization_course['title'],
+                'lecturer': lecturer_id,
+                'ViewCount': organization_course['view_count'],
+                'type': type_str,
+                'img': organization_course['img_str'],
+                'lecturer_name': lecturer_nickname,
+                'organization_logo': organization_info['logo'],
+                'duration': duration
+            }
+            return course
         else:
-            lecturer_nickname = ''
-        type_str = get_course_type_name_by_type_id(organization_course['category_id'])
-        duration = get_course_video_duration(course_id)
-        course = {
-            'id': course_id,
-            'title': organization_course['title'],
-            'lecturer': lecturer_id,
-            'ViewCount': organization_course['view_count'],
-            'type': type_str,
-            'img': organization_course['img_str'],
-            'lecturer_name': lecturer_nickname,
-            'organization_logo': organization_info['logo'],
-            'duration': duration
-        }
-        return course
+            return None
     else:
         return None
 
