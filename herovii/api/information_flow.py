cd00572c325340d9336120a62e6321d8063c424b
yangchujie <yangchujie1@163.com>
2016-04-18 17:22:26 +0800

    修改2.7首页推荐banner和资讯流文章内容;

--- herovii/api/information_flow.py
+++ herovii/api/information_flow.py
@@ -4,7 +4,7 @@ from herovii.cache import cache
 from herovii.libs.bpbase import ApiBlueprint
 from herovii.libs.util import parse_page_args, make_cache_key
 from herovii.service.information_flow import get_information_flow_banner_service, get_information_flow_content_service, \
-    get_information_flow_content_type_service
+    get_information_flow_content_type_service, get_information_flow_content_service_v2_7
 
 __author__ = 'yangchujie'
 
@@ -52,6 +52,27 @@ def get_information_flow_content():
     if 'type' in request.args:
         information_type = int(request.args.get('type'))
     total_count, data_list = get_information_flow_content_service(uid, information_type, page, per_page)
+    result = {
+        'total_count': total_count,
+        'data': data_list
+    }
+    headers = {'Content-Type': 'application/json'}
+    return json.dumps(result), 200, headers
+
+
+@api.route('/2.7/content', methods=['GET'])
+@cache.cached(timeout=120, key_prefix=make_cache_key)
+def get_information_flow_content_v2_7():
+    request_json = request.get_json(force=True, silent=True)
+    page, per_page = parse_page_args(request_json)
+    if request_json is None or 'type' not in request_json.keys():
+        information_type = 0
+    else:
+        information_type = request_json['type']
+    uid = 110
+    if 'type' in request.args:
+        information_type = int(request.args.get('type'))
+    total_count, data_list = get_information_flow_content_service_v2_7(uid, information_type, page, per_page)
     result = {
         'total_count': total_count,
         'data': data_list
