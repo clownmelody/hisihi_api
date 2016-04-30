30feb16fe047c5bb71d3427223d7ef938da47f56
yangchujie <yangchujie1@163.com>
2016-04-30 14:33:37 +0800

    修改机构头条;

--- herovii/service/news.py
+++ herovii/service/news.py
@@ -1,3 +1,4 @@
+from flask import current_app
 from herovii.libs.error_code import NotFound
 from herovii.models.news.news_org import NewsOrg
 
@@ -24,13 +25,14 @@ def get_news_dto_paginate(page, count):
 
 
 def get_org_news_dto_paginate(oid, page, count):
-    news = NewsOrg.query.filter(NewsOrg.status != -1, NewsOrg.organization_id == oid) \
+    news = NewsOrg.query(NewsOrg.id, NewsOrg.organization_id, NewsOrg.tag, NewsOrg.title).filter(NewsOrg.status != -1, NewsOrg.organization_id == oid) \
         .order_by(NewsOrg.create_time.desc()). \
         paginate(page, count).items
     total_count = NewsOrg.query.count()
     if news is None:
         raise NotFound(error='news not found', error_code=3000)
-
+    for news_info in news:
+        news_info['url'] = current_app.config['SERVER_HOST_NAME'] + '/api.php?s=/organization/noticedetail/id/' + news_info['id']
     dto_paginate = {
         'news': news,
         'total_count': total_count
@@ -39,5 +41,6 @@ def get_org_news_dto_paginate(oid, page, count):
 
 
 def get_news_org_by_id(nid):
-    news = NewsOrg.query.filter(NewsOrg.id == nid).first()
+    news = NewsOrg.query(NewsOrg.id, NewsOrg.organization_id, NewsOrg.tag, NewsOrg.title).filter(NewsOrg.id == nid).first()
+    news['url'] = current_app.config['SERVER_HOST_NAME'] + '/api.php?s=/organization/noticedetail/id/' + nid
     return news
