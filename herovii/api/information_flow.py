38db3a91eb78d026c017357e3819509293151280
yangchujie <yangchujie1@163.com>
2016-07-11 12:32:48 +0800

    修改搜索头条地址；

--- herovii/api/information_flow.py
+++ herovii/api/information_flow.py
@@ -8,7 +8,7 @@ from herovii.libs.util import parse_page_args, make_cache_key
 from herovii.service.information_flow import get_information_flow_banner_service, get_information_flow_content_service, \
     get_information_flow_content_type_service, get_information_flow_content_service_v2_7, \
     search_information_flow_content_service, get_information_flow_column_service, \
-    get_information_flow_content_service_v2_9
+    get_information_flow_content_service_v2_9, search_information_flow_content_service_v2_9
 
 __author__ = 'yangchujie'
 
@@ -141,6 +141,29 @@ def search_information_flow_content():
     if 'keywords' in request.args:
         keywords = request.args.get('keywords')
     total_count, data_list = search_information_flow_content_service(uid, keywords, page, per_page)
+    result = {
+        'total_count': total_count,
+        'data': data_list
+    }
+    headers = {'Content-Type': 'application/json'}
+    return json.dumps(result), 200, headers
+
+
+@api.route('/2.9/search', methods=['GET'])
+def search_information_flow_content_v2_9():
+    request_json = request.get_json(force=True, silent=True)
+    page, per_page = parse_page_args(request_json)
+    if not hasattr(g, 'user'):
+        uid = 0
+    else:
+        uid = g.user[0]
+    if request_json is None or 'keywords' not in request_json.keys():
+        keywords = ''
+    else:
+        keywords = request_json['keywords']
+    if 'keywords' in request.args:
+        keywords = request.args.get('keywords')
+    total_count, data_list = search_information_flow_content_service_v2_9(uid, keywords, page, per_page, 2.9)
     result = {
         'total_count': total_count,
         'data': data_list
