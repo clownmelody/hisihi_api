9d1748be3a84397ccbad7b7899d3becc690b1abb
shaolei <ysl90s@qq.com>
2016-10-12 14:20:43 +0800

    fix

--- herovii/api/pay.py
+++ herovii/api/pay.py
@@ -52,7 +52,6 @@ def create_pay_order(oid, type):
         data = {
             'data': res
         }
-        current_app.logger.warn(res)
         return json.dumps(data), 200, headers
     else:
         #微信支付
